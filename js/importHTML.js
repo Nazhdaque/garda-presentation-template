06df6d8b5ecc1217fe87667999bc965be4c8dc86
nazhdaque <nazhdaque@gmail.com>
2024-05-09 10:46:18 +0300

    cards w images, list w icons

--- js/importHTML.js
+++ js/importHTML.js
@@ -3,6 +3,8 @@ import slide_2 from "../html/slide-2.html?raw";
 import slide_3 from "../html/slide-3.html?raw";
 import slide_4 from "../html/slide-4.html?raw";
 import slide_5 from "../html/slide-5.html?raw";
+import slide_6 from "../html/slide-6.html?raw";
+import slide_7 from "../html/slide-7.html?raw";
 
 const slides = document.querySelector(".main-content");
 slides.insertAdjacentHTML("beforeend", slide_1);
@@ -10,3 +12,5 @@ slides.insertAdjacentHTML("beforeend", slide_2);
 slides.insertAdjacentHTML("beforeend", slide_3);
 slides.insertAdjacentHTML("beforeend", slide_4);
 slides.insertAdjacentHTML("beforeend", slide_5);
+slides.insertAdjacentHTML("beforeend", slide_6);
+slides.insertAdjacentHTML("beforeend", slide_7);
