4542705474352b5e20a1b7aff594603260019981
nazhdaque <nazhdaque@gmail.com>
2024-05-10 22:36:26 +0300

    Updates

--- assets/index-DEwIudNL.js
+++ assets/index-DEwIudNL.js
@@ -1,4 +1,5 @@
-(function(){const n=document.createElement("link").relList;if(n&&n.supports&&n.supports("modulepreload"))return;for(const t of document.querySelectorAll('link[rel="modulepreload"]'))e(t);new MutationObserver(t=>{for(const i of t)if(i.type==="childList")for(const o of i.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&e(o)}).observe(document,{childList:!0,subtree:!0});function l(t){const i={};return t.integrity&&(i.integrity=t.integrity),t.referrerPolicy&&(i.referrerPolicy=t.referrerPolicy),t.crossOrigin==="use-credentials"?i.credentials="include":t.crossOrigin==="anonymous"?i.credentials="omit":i.credentials="same-origin",i}function e(t){if(t.ep)return;t.ep=!0;const i=l(t);fetch(t.href,i)}})();const c=`<article class="slide __slide-1 layout-center">
+(function(){const n=document.createElement("link").relList;if(n&&n.supports&&n.supports("modulepreload"))return;for(const t of document.querySelectorAll('link[rel="modulepreload"]'))e(t);new MutationObserver(t=>{for(const i of t)if(i.type==="childList")for(const o of i.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&e(o)}).observe(document,{childList:!0,subtree:!0});function l(t){const i={};return t.integrity&&(i.integrity=t.integrity),t.referrerPolicy&&(i.referrerPolicy=t.referrerPolicy),t.crossOrigin==="use-credentials"?i.credentials="include":t.crossOrigin==="anonymous"?i.credentials="omit":i.credentials="same-origin",i}function e(t){if(t.ep)return;t.ep=!0;const i=l(t);fetch(t.href,i)}})();const c=`<article class="slide __slide-1 layout-center"
+				 id="slide-1">
 	<div class="scroll-down">
 		<div class="scroll-down__chevron"></div>
 		<div class="scroll-down__chevron"></div>
@@ -81,14 +82,15 @@
 				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel perferendis et tenetur!</p>
 			</div>
 		</section>
-</article>`,r=`<article class="slide __slide-2 layout-center">
+</article>`,r=`<article class="slide __slide-2 layout-center"
+				 id="slide-2">
 	<div class="slide__top-group">
 		<div class="slide-ttl width-x z-up">
 			<h1 class="fs-2xl fw-eb">Plain cards</h1>
 			<p class="fs-lg txt-grd">With icons</p>
 		</div>
 		<a href="#"
-			 class="brand-logo"
+			 class="brand-logo sm-d-none"
 			 aria-label="home">
 			<img src="./images/logo-b.svg"
 					 alt="brand-logo">
@@ -117,7 +119,7 @@
 			</div>
 		</section>
 
-		<section class="card-plain -flow-col">
+		<section class="card-plain flow-col">
 			<div class="img-box icon-bg">
 				<img class="o-fit-cover"
 						 loading="lazy"
@@ -181,14 +183,15 @@
 			</div>
 		</section> -->
 	</div>
-</article>`,d=`<article class="slide __slide-3 layout-center">
+</article>`,d=`<article class="slide __slide-3 layout-center"
+				 id="slide-3">
 	<div class="slide__top-group">
 		<div class="slide-ttl width-x z-up">
 			<h1 class="fs-2xl fw-eb">Framed cards</h1>
 			<p class="fs-lg txt-grd">With icons</p>
 		</div>
 		<a href="#"
-			 class="brand-logo"
+			 class="brand-logo sm-d-none"
 			 aria-label="home">
 			<img src="./images/logo-w.svg"
 					 alt="brand-logo">
@@ -265,14 +268,15 @@
 			</div>
 		</section>
 	</div>
-</article>`,m=`<article class="slide __slide-4 layout-center z-up">
+</article>`,m=`<article class="slide __slide-4 layout-center z-up"
+				 id="slide-4">
 	<div class="slide__top-group">
 		<div class="slide-ttl width-x z-up">
 			<h1 class="fs-2xl fw-eb">Ordered list</h1>
 			<p class="fs-lg txt-grd">Based on plain cards</p>
 		</div>
 		<a href="#"
-			 class="brand-logo"
+			 class="brand-logo sm-d-none"
 			 aria-label="home">
 			<img src="./images/logo-w.svg"
 					 alt="brand-logo">
@@ -289,7 +293,7 @@
 			</div>
 		</li>
 
-		<li class="card-plain -flow-col">
+		<li class="card-plain flow-col">
 			<div class="img-box icon-bg fs-2xl"></div>
 			<div class="card-plain__body">
 				<h2 class="card-plain__ttl fs-md fw-eb">Lorem ipsum dolor</h2>
@@ -323,14 +327,15 @@
 			</div>
 		</li> -->
 	</ol>
-</article>`,u=`<article class="slide __slide-5 layout-center">
+</article>`,u=`<article class="slide __slide-5 layout-center"
+				 id="slide-5">
 	<div class="slide__top-group">
 		<div class="slide-ttl width-x z-up">
 			<h1 class="fs-2xl fw-eb">Ordered list</h1>
 			<p class="fs-lg txt-grd">Based on framed cards</p>
 		</div>
 		<a href="#"
-			 class="brand-logo"
+			 class="brand-logo sm-d-none"
 			 aria-label="home">
 			<img src="./images/logo-b.svg"
 					 alt="brand-logo">
@@ -405,14 +410,15 @@
 			</div>
 		</section>
 	</div>
-</article>`,p=`<article class="slide __slide-6 layout-center z-up">
+</article>`,p=`<article class="slide __slide-6 layout-center z-up"
+				 id="slide-6">
 	<div class="slide__top-group">
 		<div class="slide-ttl width-x z-up">
 			<h1 class="fs-2xl fw-eb">Unordered list with icons</h1>
 			<p class="fs-lg txt-grd">Any icons, with titles / with no titles</p>
 		</div>
 		<a href="#"
-			 class="brand-logo"
+			 class="brand-logo sm-d-none"
 			 aria-label="home">
 			<img src="./images/logo-w.svg"
 					 alt="brand-logo">
@@ -420,7 +426,7 @@
 	</div>
 
 	<div class="grid-x width-x  cards-plain -list-icon">
-		<section class="card-plain -flow-col">
+		<section class="card-plain">
 			<div class="img-box check-mark">
 				<img class="o-fit-cover"
 						 loading="lazy"
@@ -445,7 +451,7 @@
 			</div>
 		</section>
 
-		<section class="card-plain -flow-col">
+		<section class="card-plain">
 			<div class="img-box check-mark">
 				<img class="o-fit-cover"
 						 loading="lazy"
@@ -461,7 +467,7 @@
 			</div>
 		</section>
 
-		<section class="card-plain -flow-col">
+		<section class="card-plain">
 			<div class="img-box check-mark">
 				<img class="o-fit-cover"
 						 loading="lazy"
@@ -478,7 +484,7 @@
 
 		<!-- // -->
 
-		<section class="card-plain -flow-col">
+		<section class="card-plain">
 			<div class="img-box cross-mark">
 				<img class="o-fit-cover"
 						 loading="lazy"
@@ -494,7 +500,7 @@
 			</div>
 		</section>
 
-		<section class="card-plain -flow-col">
+		<section class="card-plain">
 			<div class="img-box cross-mark">
 				<img class="o-fit-cover"
 						 loading="lazy"
@@ -508,7 +514,7 @@
 			</div>
 		</section>
 
-		<section class="card-plain -flow-col">
+		<section class="card-plain">
 			<div class="img-box cross-mark">
 				<img class="o-fit-cover"
 						 loading="lazy"
@@ -533,14 +539,15 @@
 			</div>
 		</section>
 	</div>
-</article>`,g=`<article class="slide __slide-7 layout-center z-up">
+</article>`,g=`<article class="slide __slide-7 layout-center z-up"
+				 id="slide-7">
 	<div class="slide__top-group">
 		<div class="slide-ttl width-x z-up">
 			<h1 class="fs-2xl fw-eb">Plain cards with images</h1>
 			<p class="fs-lg txt-grd">Options: figcaption, icon inside</p>
 		</div>
 		<a href="#"
-			 class="brand-logo"
+			 class="brand-logo sm-d-none"
 			 aria-label="home">
 			<img src="./images/logo-b.svg"
 					 alt="brand-logo">
@@ -579,7 +586,13 @@
 		</section>
 
 		<section class="card-plain">
-			<figure class="img-box card-plain__img order-up">
+			<div class="card-plain__body">
+				<h2 class="card-plain__ttl fs-md fw-eb">Lorem ipsum dolor</h2>
+				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores fugiat earum temporibus aspernatur
+					voluptate.</p>
+			</div>
+
+			<figure class="img-box card-plain__img">
 				<div class="img-box icon-bg z-up">
 					<img class="o-fit-cover"
 							 loading="lazy"
@@ -596,6 +609,29 @@
 			</figure>
 
 			<div class="card-plain__body">
+				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse quae optio sunt libero suscipit, voluptatum
+					blanditiis molestias a beatae. Accusamus officiis sunt, consequuntur ea quos eos!</p>
+			</div>
+		</section>
+
+		<section class="card-plain">
+			<figure class="img-box card-plain__img order-up">
+				<div class="img-box icon-bg z-up">
+					<img class="o-fit-cover"
+							 loading="lazy"
+							 decoding="async"
+							 src="./images/icons/icon-usr-38-w.svg"
+							 alt="">
+				</div>
+				<img class="o-fit-cover"
+						 loading="lazy"
+						 decoding="async"
+						 src="./images/img-3.webp"
+						 alt="">
+				<figcaption>This is figcaption</figcaption>
+			</figure>
+
+			<div class="card-plain__body">
 				<h2 class="card-plain__ttl fs-md fw-eb">Lorem ipsum dolor</h2>
 				<p><a href="#"
 						 target="_blank"
@@ -607,5 +643,104 @@
 					fugiat voluptates in, sequi reiciendis et cupiditate, nobis voluptatum.</p>
 			</div>
 		</section>
+
+		<!-- // -->
+
+
 	</div>
-</article>`,s=document.querySelector(".main-content");s.insertAdjacentHTML("beforeend",c);s.insertAdjacentHTML("beforeend",r);s.insertAdjacentHTML("beforeend",d);s.insertAdjacentHTML("beforeend",m);s.insertAdjacentHTML("beforeend",u);s.insertAdjacentHTML("beforeend",p);s.insertAdjacentHTML("beforeend",g);const v=(a,n)=>{document.querySelectorAll(`${a} ${n}`).forEach(e=>{const t=e.closest(`${a}`),i=()=>{e.style.display="none",t.removeEventListener("scrollend",i)};t.addEventListener("scrollend",i)})};v(".main-content",".scroll-down");
+</article>`,v=`<article class="slide __slide-8 layout-center z-up"
+				 id="slide-8">
+	<div class="slide__top-group">
+		<div class="slide-ttl width-x z-up">
+			<h1 class="fs-2xl fw-eb">Plain cards with images</h1>
+			<p class="fs-lg txt-grd">Horizontal</p>
+		</div>
+		<a href="#"
+			 class="brand-logo sm-d-none"
+			 aria-label="home">
+			<img src="./images/logo-b.svg"
+					 alt="brand-logo">
+		</a>
+	</div>
+
+	<div class="grid-x z-up cards-plain width-x">
+		<section class="card-plain flow-col sm-flow-row">
+			<figure class="img-box card-plain__img">
+				<img class="o-fit-cover"
+						 loading="lazy"
+						 decoding="async"
+						 src="./images/img-1.webp"
+						 alt="">
+			</figure>
+
+			<div class="card-plain__body">
+				<h2 class="card-plain__ttl fs-md fw-eb">Lorem ipsum dolor</h2>
+				<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima quis ipsum nulla hic explicabo maiores,
+					alias id facere accusamus molestiae quidem. Ab repellat sit aliquam voluptatem facere, nam reprehenderit sunt
+					iure debitis?.</p>
+			</div>
+		</section>
+
+		<section class="card-plain flow-col sm-flow-row">
+			<figure class="img-box card-plain__img order-up">
+				<img class="o-fit-cover"
+						 loading="lazy"
+						 decoding="async"
+						 src="./images/img-2.webp"
+						 alt="">
+			</figure>
+
+			<div class="card-plain__body">
+				<h2 class="card-plain__ttl fs-md fw-eb">Lorem ipsum dolor</h2>
+				<p><a href="#"
+						 target="_blank"
+						 rel="noopener noreferrer"
+						 class="txt-link">Lorem ipsum</a> dolor sit amet consectetur adipisicing elit. Recusandae voluptate
+					et voluptatibus reprehenderit
+					quibusdam quo, nobis repudiandae blanditiis dolore fuga, nisi voluptates officia facere quia doloribus
+					repellat autem, eius eaque accusamus alias. Natus architecto quas quidem fuga amet aspernatur labore nulla,
+					fugiat voluptates in, sequi reiciendis et cupiditate, nobis voluptatum.</p>
+			</div>
+		</section>
+
+		<section class="card-plain flow-col sm-flow-row grid-full-width">
+			<div class="card-plain__body">
+				<h2 class="card-plain__ttl fs-md fw-eb">Lorem ipsum dolor</h2>
+				<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima quis ipsum nulla hic explicabo maiores,
+					alias id facere accusamus molestiae quidem. Ab repellat sit aliquam voluptatem facere, nam reprehenderit sunt
+					iure debitis?.</p>
+				<ul class="list-bullet">
+					<li>
+						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga iusto magni eos voluptates in?</p>
+					</li>
+					<li>
+						<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor corrupti deleniti ab, consequuntur quas
+							eum similique excepturi fugit ea expedita dignissimos possimus eligendi!</p>
+					</li>
+					<li>
+						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim iure dicta impedit eos ab odio, veritatis
+							reiciendis!</p>
+					</li>
+				</ul>
+			</div>
+
+			<figure class="img-box card-plain__img">
+				<img class="o-fit-cover"
+						 loading="lazy"
+						 decoding="async"
+						 src="./images/img-3.webp"
+						 alt="">
+			</figure>
+
+			<div class="card-plain__body">
+				<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam tenetur ut esse ea sit culpa accusamus?
+					Harum accusamus odit iure quas repellendus ab tenetur natus ipsa repudiandae?</p>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum saepe, corporis optio hic modi quos
+					repellendus, voluptatem mollitia officia iure enim illo quisquam molestiae minima ut non. Omnis et quas
+					necessitatibus perspiciatis dignissimos debitis reprehenderit amet! Nihil, provident.</p>
+				<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eveniet soluta atque dolores. Illo vitae adipisci
+					dolor. Incidunt ex alias adipisci ipsa quae quia cum odio, asperiores recusandae, quis id!</p>
+			</div>
+		</section>
+	</div>
+</article>`,s=document.querySelector(".main-content");s.insertAdjacentHTML("beforeend",c);s.insertAdjacentHTML("beforeend",r);s.insertAdjacentHTML("beforeend",d);s.insertAdjacentHTML("beforeend",m);s.insertAdjacentHTML("beforeend",u);s.insertAdjacentHTML("beforeend",p);s.insertAdjacentHTML("beforeend",g);s.insertAdjacentHTML("beforeend",v);const f=(a,n)=>{document.querySelectorAll(`${a} ${n}`).forEach(e=>{const t=e.closest(`${a}`),i=()=>{e.style.display="none",t.removeEventListener("scrollend",i)};t.addEventListener("scrollend",i)})};f(".main-content",".scroll-down");
