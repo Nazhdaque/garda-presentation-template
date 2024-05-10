e4d7f21453aadb6a273f02ead9b15d53026a721a
nazhdaque <nazhdaque@gmail.com>
2024-05-10 22:35:35 +0300

    nav-panel

--- js/importHTML.js
+++ js/importHTML.js
@@ -5,6 +5,7 @@ import slide_4 from "../html/slide-4.html?raw";
 import slide_5 from "../html/slide-5.html?raw";
 import slide_6 from "../html/slide-6.html?raw";
 import slide_7 from "../html/slide-7.html?raw";
+import slide_8 from "../html/slide-8.html?raw";
 
 const slides = document.querySelector(".main-content");
 slides.insertAdjacentHTML("beforeend", slide_1);
@@ -14,3 +15,4 @@ slides.insertAdjacentHTML("beforeend", slide_4);
 slides.insertAdjacentHTML("beforeend", slide_5);
 slides.insertAdjacentHTML("beforeend", slide_6);
 slides.insertAdjacentHTML("beforeend", slide_7);
+slides.insertAdjacentHTML("beforeend", slide_8);
